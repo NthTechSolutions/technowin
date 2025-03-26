893e9aee1acf9da3adbe354795d950e76d284dd8
technowin <technowinitinfra@gmail.com>
2025-03-26 11:32:04 +0530

    STATICFILES_DIRS

--- technowin/settings.py
+++ technowin/settings.py
@@ -17,6 +17,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Quick-start development settings - unsuitable for production
@@ -131,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Ensure this matches your folder structure
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",  # Ensure this matches your folder structure
+# ]
 
 
 # Default primary key field type
